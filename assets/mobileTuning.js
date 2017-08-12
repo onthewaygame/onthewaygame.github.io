14700850c813fcfca029bc744509c7c469183d72
freeman <uvarov.b@gmail.com>
2017-08-12 18:05:43 +0300

    hide mobile or desktop elements

--- assets/mobileTuning.js
+++ assets/mobileTuning.js
@@ -1,10 +1,14 @@
 //var isMobile = screen.width < 768;
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 if (isMobile) {
-    $(".slideLink").each(function () {
-        $(this).addClass("nonePointerEvents");
+    $(".desktop").each(function () {
+        $(this).hide();
     });
 } else {
+     $(".mobile").each(function () {
+        $(this).hide();
+    });
+    
     $("a").each(function () {
         $(this).addClass("linkDecoration");
     });
